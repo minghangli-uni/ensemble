38782aaf785fee3077be4a7dbb463ef788cb4871
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2021-01-05 18:36:35 +1100

    WIP, mostly working now

--- ensemble.py
+++ ensemble.py
@@ -11,10 +11,11 @@ Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.txt
 from __future__ import print_function
 # import sys
 import os
-import subprocess
+import shutil
+# import subprocess
 import git
 import numpy as np
-# import glob  # BUG: fails if payu module loaded - some sort of module clash with re
+import glob  # BUG: fails if payu module loaded - some sort of module clash with re
 # import itertools
 try:
     import yaml
@@ -30,37 +31,83 @@ def ensemble(yamlfile='ensemble.yaml'):
     '''
     Create an ensemble by varying only one parameter at a time.
     '''
-    # could loop over all values of all parameters using `itertools.product` - see https://stackoverflow.com/questions/1280667/in-python-is-there-an-easier-way-to-write-6-nested-for-loops
+    # alternatively, could loop over all values of all parameters using `itertools.product` - see https://stackoverflow.com/questions/1280667/in-python-is-there-an-easier-way-to-write-6-nested-for-loops
     indata = yaml.load(open(yamlfile, 'r'), Loader=yaml.SafeLoader)
     template = indata['template']
     templaterepo = git.Repo(template)
-    print(indata)
     for fname, nmls in indata['namelists'].items():
         for group, names in nmls.items():
             for name, values in names.items():
-                print(fname, group, name, values)
                 for v in values:
-                    expname = '_'.join([template, name, str(v)])
-                    if os.path.exists(expname):
-                        print(expname, 'exists; skipping')
+                    exppath = os.path.join(os.getcwd(), '_'.join([template, name, str(v)]))
+                    relexppath = os.path.relpath(exppath, os.getcwd())
+                    expname = os.path.basename(relexppath)
+                    if os.path.exists(exppath):
+                        print(' -- skipping', relexppath, '- already exists')
                     else:
-                        print('creating', expname)
-                        exprepo = templaterepo.clone(expname)
-                        # TODO: fix up remotes, set up new branch
-                        fpath = os.path.join(expname, fname)
-                        if set(fname, group, name) == set('ice/cice_in.nml', 'dynamics_nml', 'turning_angle'):
-                            f90nml.patch(fpath, {group: {'cosw': np.cos(v * np.pi / 180. )}})
-                            f90nml.patch(fpath, {group: {'sinw': np.sin(v * np.pi / 180. )}})
+                        print('creating', relexppath)
+                        exprepo = templaterepo.clone(exppath)
+                        fpath = os.path.join(exppath, fname)
+                        if set([fname, group, name]) == set(['ice/cice_in.nml', 'dynamics_nml', 'turning_angle']):
+                            f90nml.patch(fpath, {group: {'cosw': np.cos(v * np.pi / 180. )}}, fpath+'_tmp2')
+                            f90nml.patch(fpath+'_tmp2', {group: {'sinw': np.sin(v * np.pi / 180. )}}, fpath+'_tmp')
+                            os.remove(fpath+'_tmp2')
+                        else:  # general case
+                            f90nml.patch(fpath, {group: {name: v}}, fpath+'_tmp')
+                        os.rename(fpath+'_tmp', fpath)
+                        if not exprepo.is_dirty():
+                            print(' *** deleting', relexppath, '- parameters are identical to', template)
+                            shutil.rmtree(exppath)
                         else:
-                            f90nml.patch(fpath, {group: {name: v}}) #, fpath)
-                        # TODO: commit changes
-                        
-                        # with open(fname) as nml_file:
-                        #     nml = f90nml.read(nml_file)
-                        # 
-                        #     patch_nml = {group: {name: v}}
-                        #     nml[group][name] = v
-                        #     nml.write(nml_file)
+                            # TODO:
+                            # - update metadata.yaml
+                            # mkdir /scratch/x77/aek156/access-om2/archive/01deg_jra55v140_iaf_cycle3
+                            # ln -s /scratch/x77/aek156/access-om2/archive/01deg_jra55v140_iaf_cycle3 archive
+                            # cp -r /scratch/x77/aek156/access-om2/archive/01deg_jra55v140_iaf_cycle2/restart487 archive/restart487
+                            # ln -s /scratch/x77/aek156/access-om2/archive/01deg_jra55v140_iaf_cycle2/output487 archive/output487
+
+                            # set SYNCDIR in sync_data.sh
+                            sdpath = os.path.join(exppath, 'sync_data.sh')
+                            with open(sdpath+'_tmp', 'w') as wf:
+                                with open(sdpath, 'r') as rf:
+                                    for line in rf:
+                                        if line.startswith('SYNCDIR='):
+                                            syncbase = os.path.dirname(line[len('SYNCDIR='):])
+                                            syncdir = os.path.join(syncbase, expname)
+                                            wf.write('SYNCDIR='+syncdir+'\n')
+                                        else:
+                                            wf.write(line)
+                            os.rename(sdpath+'_tmp', sdpath)
+
+                            if os.path.exists(syncdir):
+                                print(' *** deleting', relexppath, '- SYNCDIR', syncdir, 'already exists')
+                                shutil.rmtree(exppath)
+                            else:
+
+                                # set jobname in config.yaml to '_'.join([name, str(v)])
+                                # don't use yaml package as it doesn't preserve comments and ordering
+                                configpath = os.path.join(exppath, 'config.yaml')
+                                with open(configpath+'_tmp', 'w') as wf:
+                                    with open(configpath, 'r') as rf:
+                                        for line in rf:
+                                            if line.startswith('jobname:'):
+                                                wf.write('jobname: '+'_'.join([name, str(v)])+'\n')
+                                            else:
+                                                wf.write(line)
+                                os.rename(configpath+'_tmp', configpath)
+
+# TODO: finish this bit
+                                runfrom = os.path.join(os.path.realpath(os.path.join(template, 'archive')), indata['runfrom'])
+                                print(runfrom)
+                                for f in glob.glob(os.path.join(exppath, 'run_summary_*.csv')):
+                                    exprepo.git.rm(os.path.basename(f))
+
+                            # fix up remotes, set up new branch, commit
+                                exprepo.remotes.origin.rename('source')
+                                exprepo.create_remote('origin', templaterepo.remotes.origin.url)
+                                exprepo.git.checkout('HEAD', b=expname) # switch to a new branch
+                                # exprepo.git.commit(a=True, m='set up '+expname)
+
 
 if __name__ == '__main__':
     import argparse
